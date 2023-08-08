8e471dcd4394675bbe4af1b9e22519512df4eb1d
Vembarasan Nagarajan <vembarasugn@gmil.com>
2023-08-08 20:51:12 +0530

    Routing fix issue 3

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,10 @@
 import Image from 'next/image';
-//import About from './aboutus/page';
 import Landingpage from '/src/components/Landingpage/page';
-//import Services from '/src/app/services/page';
-//import Projects from '/src/app/projects/page';
-//import Contact from './contactus/page';
 
 function Home() {
   return (   
   <div className="bg-white-300">
      <Landingpage/>
-     {/* <About/>
-     <Services/>
-     <Projects/>
-     <Contact/>   */}
   </div>
   )
 };
