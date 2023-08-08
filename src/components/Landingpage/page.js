cd797b4d828b537d43db3d42d2a6b956b4359bcb
Vembarasan Nagarajan <vembarasugn@gmil.com>
2023-08-09 01:53:50 +0530

    Landing page updated

--- src/components/Landingpage/page.js
+++ src/components/Landingpage/page.js
@@ -10,7 +10,7 @@ const Landingpage = () => {
         <div className="flex flex-col w-full xl:w-2/5 justify-center lg:items-start overflow-y-hidden">
           <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold leading-tight text-center md:text-left">
             Vtech
-            <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-gray-500 to-purple-500">
+            <span className="bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-gray-500">
             Renewable
             </span><br/>
              services!  
@@ -32,7 +32,7 @@ const Landingpage = () => {
             </div>
             <div className="flex items-center justify-between pt-4">
               <button
-                className="bg-gradient-to-r from-purple-500 to-green-800 hover:from-gray-500 hover:to-green-700 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-80  ease-in-out"
+                className="bg-gradient-to-r to-green-800 hover:from-gray-500 hover:to-green-700 text-white font-bold py-2 px-4 rounded focus:ring transform transition hover:scale-80  ease-in-out"
                 type="button"
               >
                Contact Us
